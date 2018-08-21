7bea9f455b941c86697b31c138f726986b9aa8b4
Anthony Calabretta <AryToNeX@users.noreply.github.com>
2018-08-21 20:15:15 +0200

    Make dirs

--- install.sh
+++ install.sh
@@ -6,10 +6,12 @@ BUILDNUM=$(wget -q -O- http://tony0000.altervista.org/RGBDuino/currentbuild.txt)
 
 echo "Pulling version $BUILDNUM"
 
+mkdir "/home/$(whoami)/.local/share/RGBDuino"
 wget -q "http://tony0000.altervista.org/RGBDuino/builds/$BUILDNUM/RGBDuino.phar" -O "/home/$(whoami)/.local/share/RGBDuino/RGBDuino.phar"
 wget -q "http://tony0000.altervista.org/RGBDuino/builds/$BUILDNUM/rgbduino" -O "/home/$(whoami)/.local/share/RGBDuino/rgbduino"
 echo ${BUILDNUM} | tee "/home/$(whoami)/.local/share/RGBDuino/current-build" 1> /dev/null
 
+mkdir "/home/$(whoami)/.local/bin"
 echo "#!/bin/bash
 
 php /home/$(whoami)/.local/share/RGBDuino/rgbduino \"\$@\"
